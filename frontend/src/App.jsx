680423999cf93d6b0b2bcd644984191f0e280d34
SelamB <selambeyene2@gmail.com>
2024-05-23 12:30:17 -0700

    sets up BinaryConvo component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { BinaryConvo } from './Components/BinaryConvo';
+
+function App() {
+  return (
+    <div className="App">
+      <BinaryConvo />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/BinaryConvo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BinaryConvo = () => {
+  return (
+    <div>BinaryConvo</div>
+  )
+}
